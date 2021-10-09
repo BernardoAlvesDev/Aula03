605befdc8bfe8d0667d3980de8c64d2e2b484762
Bernardo Nascimento <bernardoalves1404@gmail.com>
2021-10-08 21:01:20 -0300

    if Não

--- src/OperadorNao.java
+++ src/OperadorNao.java
@@ -10,7 +10,11 @@ public class OperadorNao {
 		tipoUsuario = leitor.next();
 		
 		//Aqui é preciso criar um if que verifique se o tipo de usuário digitado é DIFERENTE de "administrador"
-		
+		if(!tipoUsuario.equals("administrador")) {
+			System.out.println("Atenção, usuário sem poderes administrativos");
+		}else {
+			System.out.println("Acesso liberado " + tipoUsuario);
+		}
 		leitor.close();
 		
 
